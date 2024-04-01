c1f23156474af504527477fa3213ec8de5eda56f
Priyanshu Yadav <priyanshu.yadav@ksolves.com>
2024-04-01 15:10:22 +0530

    delete bonus context

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ import Reward from "./components/Reward";
 import Login from "./components/Login";
 import { UserProvider } from "./context/UserContext";
 import { MyContextProvider } from "./context/MyContext";
-import { BonusContextProvider } from "./context/BonusContext";
 import Admin from "./components/Admin";
 
 function App() {
   const { amount } = useSelector((state) => state.account); //ye account jo store ke reducer me dala h vhi h
   const { points } = useSelector((state) => state.bonus);
 
+  console.log(window.location)
+
+
   return (
     <UserProvider>
       <MyContextProvider>
-        <BonusContextProvider>
 
       <div className="App">
         <h4>App</h4>
@@ -29,7 +30,6 @@ function App() {
         <Reward></Reward>
         <Admin></Admin>
       </div>
-        </BonusContextProvider>
       </MyContextProvider>
     </UserProvider>
   );
--- src/components/Bonus.js
+++ src/components/Bonus.js
@@ -1,16 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { increment } from '../slices/bonusSlice';
-import { useContext } from 'react';
-import { BonusContext } from '../context/BonusContext';
-
 
 function Bonus() {
   const {points} = useSelector(state=>state.bonus)
   const dispatch = useDispatch()
-  const {bonus,setBonus} = useContext(BonusContext)
-  const incrementBonusContextValue = () =>{
-    setBonus(bonus+1)
-  }
+
 
   return (
     <div className="card">
@@ -21,7 +15,6 @@ function Bonus() {
         <h3>Total Point : ${points}</h3>
 
         <button onClick={()=>dispatch(increment())}>Increment +</button>
-        <button onClick={incrementBonusContextValue}>Increment +</button>
       </div>
     </div>
   );
